ec85039f60e48006700c38ae1b552a2ddd030b8f
Pedro Dellazzari <pedrocdellazzari@gmail.com>
2023-08-25 17:25:15 -0300

    Fazendo a condicional dentro da funcao do mongo

--- Mongo_functions.py
+++ Mongo_functions.py
@@ -1,11 +1,21 @@
 #bibliotecas 
 import pymongo
 
+##Endereço de IPS da marca
+GoPro_IP = '54.207.87.136'
+Wacom_IP = '52.67.113.63'
+
 #função para inserir dados no mongo
-def insert_data(database_name, collection, data_dict):
+def insert_data(database_name, collection, data_dict, brand):
+
+    #Fazendo as condicionais para a marca
+    if brand == 'GoPro':
+        IP = GoPro_IP
+    elif brand == 'Wacom':
+        IP = Wacom_IP
 
     #conectando ao mongo    
-    client = pymongo.MongoClient("mongodb://mongodbadmin:admin@54.207.87.136:27017/?authMechanism=DEFAULT")
+    client = pymongo.MongoClient(f"mongodb://mongodbadmin:admin@{IP}:27017/?authMechanism=DEFAULT")
     
     #Acessando o database
     db = client[database_name]
