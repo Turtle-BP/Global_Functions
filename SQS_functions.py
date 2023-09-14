b90f7653ed84f427340ea570a1ec49972fd47e40
PedroDellazzari-Turtle <pdellazzari@turtlebp.com>
2023-09-14 14:44:47 -0300

    Adicionando o message_data na funcao de sqs para criar o dic antes de chamar a mesma

--- SQS_functions.py
+++ SQS_functions.py
@@ -2,24 +2,14 @@
 import boto3
 import json 
 
-#Criando as credenciais da AWS
-AWS_ACCESS_KEY_ID = 'AKIAXJ44CIZUYE4UTUH4'
-AWS_SECRET_ACCESS_KEY = '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'
-
 #Função para enviar mensagem para a fila 
-def send_sqs_message(sqs_queue_url, marca, produto, url):
+def send_sqs_message(sqs_queue_url, message_data):
+    
     # Create SQS client
-    sqs_client = boto3.client('sqs', region_name='sa-east-1', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-
-    #Criand a mensagem em formato de json 
-    message_data = {
-        'marca':marca,
-        'produto':produto,
-        'url':url,
-    }
+    sqs_client = boto3.client('sqs', region_name='sa-east-1')
 
     # Convert message_data into JSON
-    message_data = json.dumps(message_data)
+    message_data = json.dumps(message_data, ensure_ascii=False)
 
     # Send message to SQS queue
     send_msg_response = sqs_client.send_message(
@@ -27,7 +17,6 @@ def send_sqs_message(sqs_queue_url, marca, produto, url):
         MessageBody=message_data
     )
 
+    print("Mensagem enviada com sucesso")
+
     return send_msg_response
-
-
-#Função para receber mensagem da fila
