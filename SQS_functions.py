52b970bc7ac4061c4ceeb527757194329948f081
PedroDellazzari-Turtle <pdellazzari@turtlebp.com>
2023-09-19 16:16:24 -0300

    Arrumando para pegar 100% todas as mensagens de um sqs

--- SQS_functions.py
+++ SQS_functions.py
@@ -25,28 +25,44 @@ def send_sqs_message(sqs_queue_url, message_data):
 
 #Função para pegar todas as mensagens de uma única fila e transformar em um DataFrame
 def get_all_sqs(sqs_queue_url):
-        
-    # Create SQS client
-    sqs_client = boto3.client('sqs', region_name='sa-east-1')
 
-    # Receive message from SQS queue
-    response = sqs_client.receive_message(
-        QueueUrl=sqs_queue_url,
-        AttributeNames=[
-            'All'
-        ],
-        MaxNumberOfMessages=10
-    )
+    # Inicialize o cliente SQS
+    sqs = boto3.client('sqs')
 
-    messages = response.get('Messages', [])
+    # Obtenha a URL da fila com base no nome
+    response = sqs.get_queue_url(QueueName=sqs_queue_url)
+    queue_url = response['QueueUrl']
 
-    data = []
-    for message in messages:
-        body = json.loads(message['Body'])
-        data.append(body)
+    # Inicialize uma lista para armazenar todas as mensagens
+    all_messages = []
 
-    df = pd.DataFrame(data)
+    while True:
+        # Receba mensagens da fila SQS
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            AttributeNames=[
+                'All'
+            ],
+            MaxNumberOfMessages=10  # Defina o número máximo de mensagens a serem recebidas de uma vez
+        )
 
-    print("Mensagem coletadas com sucesso")
+        messages = response.get('Messages', [])
+
+        if not messages:
+            break  # Saia do loop quando não houver mais mensagens na fila
+
+        # Transforme as mensagens em um DataFrame
+        data = []
+        for message in messages:
+            body = json.loads(message['Body'])
+            data.append(body)
+
+        all_messages.extend(data)
+
+        # Exclua as mensagens processadas da fila
+        entries = [{'Id': message['MessageId'], 'ReceiptHandle': message['ReceiptHandle']} for message in messages]
+        sqs.delete_message_batch(QueueUrl=queue_url, Entries=entries)
+
+    df = pd.DataFrame(all_messages)
 
     return df
