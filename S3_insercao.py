514ec609bcd1ad53b37029873b6db46ef4ef8cb8
RakelTurtle <115583400+RakelTurtle@users.noreply.github.com>
2023-10-20 14:25:34 -0300

    Add função de inserção dos dataframes no s3

--- S3_insercao.py
+++ S3_insercao.py
@@ -35,3 +35,25 @@ def inserir_s3(brand, ExcelName, S3Name, S3Date, Ecommerce=False):
         #print
         print("Arquivo inserido no S3 com sucesso!")
 
+
+# Enviando catalogo para o s3
+def send_dataframe_s3(dataframe, brand, file_name):
+
+    print(dataframe)
+
+    # Verifique se existem registros a serem salvos no S3
+    if not dataframe.empty:
+        
+        # Salve esse DataFrame no Amazon S3
+        s3_client = boto3.client('s3')
+        bucket_name = 'turtle-brand-protection'
+        s3_file_key = f'Catalog_MercadoLivre/{brand}/{file_name}'
+
+        # Converta o DataFrame para um arquivo CSV
+        file_csv = dataframe.to_csv(index=False)
+
+        # Envie o arquivo CSV para o S3
+        s3_client.put_object(Bucket=bucket_name, Key=s3_file_key, Body=file_csv)
+
+        print(f'DataFrame com registros CATALOG salvo no S3: s3://{bucket_name}/{s3_file_key}')
+
